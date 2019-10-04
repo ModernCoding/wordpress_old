d08f90f8b9ee39c314647c0f38bee5ab6a95e4f0
Cormorant Programming <cormorant.programming@gmail.com>
2019-10-04 11:13:53 +0700

    Custom post types

--- wp-content/themes/moderncoding/functions.php
+++ wp-content/themes/moderncoding/functions.php
@@ -1,40 +1,105 @@
 <?php 
 
-//  adding js & css files
+  //  adding js & css files
 
-function gt_setup(){
+  function gt_setup(){
+    
+    wp_enqueue_style(
+      'google-fonts',  // alias
+      'https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Slab'
+    );
+
+    wp_enqueue_style(
+      'fontawesome',  // alias
+      'https://use.fontawesome.com/releases/v5.1.0/css/all.css'
+    );
+
+    wp_enqueue_style(
+      'style',  // alias
+      get_stylesheet_uri(),   // path to the style.css
+      NULL,   // dependencies
+      microtime(),  // stylesheet version
+      all   // media supported
+    );
+
+    wp_enqueue_script(
+      'main',
+      get_theme_file_uri('/js/main.js'),
+      NULL,
+      microtime(),
+      true  // scripts to be loaded in the footer
+    );
+
+  }
+
+
+  add_action(
+    'wp_enqueue_scripts',   // alias
+    'gt_setup'  // name of the function to run
+  );
+
+
+  //  adding theme support
+
+  function gt_init() {
+    
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
+    
+    add_theme_support(
+      'html5',
+      array('comment-list', 'comment-form', 'search-form')
+    );
   
-  wp_enqueue_style(
-    'google-fonts',  // alias
-    'https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Slab'
+  }
+
+
+  add_action(
+    'after_setup_theme',   // alias
+    'gt_init'  // name of the function to run
   );
 
-  wp_enqueue_style(
-    'fontawesome',  // alias
-    'https://use.fontawesome.com/releases/v5.1.0/css/all.css'
+
+  //  projects post type
+
+  function gt_custom_post_type() {
+    
+    register_post_type(
+
+      'project',
+
+      array(
+        'rewrite'       =>  array('slug' => 'projects'),
+
+        'labels'        =>  array(
+          'name'            =>  'Projects',
+          'singular_name'   =>  'Project',
+          'add_new_item'    =>  'Add New Project',
+          'edit_item'       =>  'Edit Project'
+        ),
+
+        'menu-icon'     =>  'dashicons-clipboard',
+        'public'        =>  true,
+        'has_archive'   =>  true,
+
+        'supports'      =>  array(
+          'title',
+          'thumbnail',
+          'editor',
+          'excerpt',
+          'comments'
+        )
+      )
+
+    );
+  
+  }
+
+
+  add_action(
+    'init',   // alias
+    'gt_custom_post_type'  // name of the function to run
   );
 
-  wp_enqueue_style(
-    'style',  // alias
-    get_stylesheet_uri(),   // path to the style.css
-    NULL,   // dependencies
-    microtime(),  // stylesheet version
-    all   // media supported
-  );
-
-  wp_enqueue_script(
-    'main',
-    get_theme_file_uri('/js/main.js'),
-    NULL,
-    microtime(),
-    true  // scripts to be loaded in the footer
-  );
-
-}
-
-add_action(
-  'wp_enqueue_scripts',   // alias
-  'gt_setup'  // name of the function to run
-);
 
 ?>
